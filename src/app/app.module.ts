1d5a9916391cc375ef65f6a7f0417047e6304ef9
Jai Gupta <jaigupta1998@gmail.com>
2020-08-12 00:00:15 +0530

    person commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule,
